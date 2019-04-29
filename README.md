a754a402aaf0e58332954f9dd34dccb877325b87
StanimalTheMan <sjc591@nyu.edu>
2019-04-29 02:09:50 -0400

    Rudimentary program that helps set  your lineup for fantasy baseball

---
+++ README.md
@@ -0,0 +1,11 @@
+# About
+This is a simple program that will let you set your Yahoo fantasy baseball lineup in advance by a number of a days you specify so you won't take L's from your opponents by default during that time.  Winning fantasy is hard even when you have your
+best baseball players play the best on the other team. 
+
+# Help
+If you can / want, feel free to implement the program for other browsers as this program uses Firefox as the browser.  Perhaps, using the idea of this program to automate the process of setting your lineups in other Fantasy providers such as ESPN and CBS Sports can get you going.  So far, it assumes you only have one fantasy team.  Perhaps making the program headless will enable you
+to set your lineups without seeing everything happen e.g. your browser opening and the entire automation happening before your eyes.  There does not seem to be an easy way
+to use a different unit of time. 
+
+# License
+Feel free to abide by the license and go above and beyond.
---
+++ mlb_start_active_players.py
@@ -0,0 +1,51 @@
+import time
+import click
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+@click.command()
+@click.option('--days', type=int, prompt="# of days you want to automate your fantasy lineup ", help="# of days you want to make sure your active players are playing")
+@click.option('--username', prompt="Your Yahoo username ", help="Your Yahoo account username: ")
+@click.option('--password', prompt="Your Yahoo password ", help="Your Yahoo account password: ")
+#@click.option('--teamname', prompt="Your teamname ", help="Name of your fantasy team that you want to set lineup for ")
+def start_active_players(days, username, password):
+    profile = webdriver.FirefoxProfile()
+    profile.accept_untrusted_certs = True
+    browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver", firefox_profile = profile)
+
+    browser.get('https://login.yahoo.com/config/login?.src=spt&.intl=us&.done=https%3A%2F%2Fbaseball.fantasysports.yahoo.com%2Fb1%3F.scrumb%3D0&specId=usernameRegWithName')
+
+    browser.find_element_by_id('login-username').send_keys(username)
+    browser.find_element_by_id('login-signin').click()
+    time.sleep(2)
+    browser.find_element_by_id('login-passwd').send_keys(password)
+    browser.find_element_by_name('verifyPassword').click()
+
+    '''
+    # locate hidden dropdown menu upon login to Yahoo Fantasy Baseball
+    teams = browser.find_element_by_xpath("//li[@class = 'Navitem Navitem-main Navitem-fantasy Va-top Fl-start Topstart']")
+    hover = ActionChains(driver).move_to_element(teams)
+    hover.perform()
+    time.sleep(1)
+
+    hover.find_element_by_xpath("//a[text() = '"+ teamname +"']").click()
+    time.sleep(2)
+    '''
+    browser.find_element_by_class_name('F-link').click()
+
+    for day in range(days):
+        browser.find_element_by_xpath("//a[text() = 'Start Active Players']").click()
+        time.sleep(2)
+        browser.find_element_by_xpath("//a[text() = 'Close']").click()
+        time.sleep(2)
+        browser.find_element_by_xpath("//a[contains(@class, 'Js-next')]").click()
+        time.sleep(2)
+
+    browser.quit()
+
+
+
+if __name__ == '__main__':
+    start_active_players()
